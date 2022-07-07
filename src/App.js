2b51f293bf065886800a9a573ccdabb9f041a1e3
Nguyen <giaanguyen221197@gmail.com>
2022-07-07 14:55:52 +0700

    Fully Intergrate Clarifai API

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
     super();
     this.state = {
       input: '',
+      box: {},
     };
   }
 
@@ -21,27 +22,56 @@ class App extends React.Component {
     this.setState({ input: e.target.value });
   };
 
-  onSubmit = () => {
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data?.outputs[0]?.data?.regions[0]?.region_info?.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = (box) => {
+    this.setState({ box });
+  };
+
+  onSubmit = async () => {
     const { input } = this.state;
 
+    this.setState({ box: null }, () => {
+      this.setState({ imageUrl: input });
+    });
+    
     const apiKey = process.env.REACT_APP_CLARIFAI_API_KEY;
     const app = new Clarifai.App({
-      apiKey
+      apiKey,
     });
 
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, input).then(
-      (res) => {
-        console.log('res', res);
-        this.setState({ imageUrl: input });
-      },
-      (err) => {
-        console.log('err', err);
-      }
-    );
+    try {
+      const response = await app.models.predict(
+        Clarifai.FACE_DETECT_MODEL,
+        input
+      );
+      this.displayFaceBox(this.calculateFaceLocation(response));
+    } catch (err) {
+      console.log('err', err);
+    }
+  };
+
+  onEnter = (e) => {
+    if (e.keyCode == 13 && e.target.value) {
+      this.onSubmit();
+    }
   };
 
   render() {
-    const { imageUrl } = this.state;
+    const { imageUrl, box } = this.state;
     return (
       <div className="App">
         <Particles
@@ -57,8 +87,9 @@ class App extends React.Component {
         <ImageLinkForm
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onSubmit}
+          onEnter={this.onEnter}
         />
-        {imageUrl && <FaceRecognition src={imageUrl} />}
+        {imageUrl && <FaceRecognition src={imageUrl} box={box} />}
       </div>
     );
   }
--- src/components/FaceRecognition/index.jsx
+++ src/components/FaceRecognition/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-
-const FaceRecognition = ({ src }) => {
+import './style.scss';
+const FaceRecognition = ({ src, box }) => {
   return (
     <div className="center ma">
       <div className="absolute mt2">
-        <img src={src} alt="face-img" width="500px" height="auto" />
+        <img
+          id="inputimage"
+          src={src}
+          alt="face-img"
+          width="500px"
+          height="auto"
+        />
+        <div
+          className="bounding-box"
+          style={{
+            top: box?.topRow,
+            right: box?.rightCol,
+            bottom: box?.bottomRow,
+            left: box?.leftCol,
+          }}
+        ></div>
       </div>
     </div>
   );
